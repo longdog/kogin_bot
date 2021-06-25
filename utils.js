903050987f17c23982d8d9865fc9f7d665cd89d2
longdog <linda@longdog.us>
2021-06-25 17:14:12 +0400

    #3 - refactor generatePattern function

--- utils.js
+++ utils.js
@@ -51,45 +51,95 @@ function getStitchCount(max) {
   return Math.floor(Math.random() * max) + 1;
 }
 
+function getLine1(len) {
+  let strLine = "0".repeat(3);
+  let curLen = 0;
+  let prevSpace = true;
+  while (true) {
+    if (len - curLen === 1 && prevSpace) {
+      strLine += "1";
+      break;
+    }
+    if (len - curLen <= 1) break;
+    if (isTrue() && prevSpace) {
+      const sc = getStitchCount(len - curLen);
+      strLine += sc.toString(20);
+      curLen += sc + 1;
+      prevSpace = false;
+    } else {
+      if (!prevSpace) {
+        ++curLen;
+      }
+      strLine += "0";
+      ++curLen;
+      prevSpace = true;
+    }
+  }
+  return strLine;
+}
+
+function getLine(len) {
+  //let strLine = "0".repeat(3);
+  // let curLen = 0;
+  // let prevSpace = true;
+  const genLine = (curLen, prevSpace) => {
+    if (len - curLen === 1 && prevSpace) {
+      return "1";
+    }
+    if (len - curLen <= 1) return "";
+    if (isTrue() && prevSpace) {
+      const sc = getStitchCount(len - curLen);
+      return sc.toString(20) + genLine(curLen + sc + 1, false);
+      // strLine += sc.toString(20);
+      // curLen += sc + 1;
+      // prevSpace = false;
+    } else {
+      return "0" + genLine(curLen + (prevSpace ? 1 : 2), true);
+      // if (!prevSpace) {
+      //   ++curLen;
+      // }
+      // strLine += "0";
+      // ++curLen;
+      // prevSpace = true;
+    }
+  };
+  let ln = genLine(0, true);
+  return "0".repeat(3) + ln;
+  ////
+  // while (true) {
+  //   if (len - curLen === 1 && prevSpace) {
+  //     strLine += "1";
+  //     break;
+  //   }
+  //   if (len - curLen <= 1) break;
+  //   if (isTrue() && prevSpace) {
+  //     const sc = getStitchCount(len - curLen);
+  //     strLine += sc.toString(20);
+  //     curLen += sc + 1;
+  //     prevSpace = false;
+  //   } else {
+  //     if (!prevSpace) {
+  //       ++curLen;
+  //     }
+  //     strLine += "0";
+  //     ++curLen;
+  //     prevSpace = true;
+  //   }
+  // }
+  // return strLine;
+}
+
 function generatePattern(isSymmetric = true) {
   let str = "";
   let len = 1;
-  for (let i = 0; i < (isSymmetric ? 9 : 17); i++) {
-    if (i === 9) {
-      len -= 4;
-    }
-    let strLine = "0".repeat(3);
-    let curLen = 0;
-    let prevSpace = true;
-    while (true) {
-      if (len - curLen === 1 && prevSpace) {
-        strLine += "1";
-        break;
-      }
-      if (len - curLen <= 1) break;
-      if (isTrue() && prevSpace) {
-        const sc = getStitchCount(len - curLen);
-        strLine += sc.toString(20);
-        curLen += sc + 1;
-        prevSpace = false;
-      } else {
-        if (!prevSpace) {
-          ++curLen;
-        }
-        strLine += "0";
-        ++curLen;
-        prevSpace = true;
-      }
-    }
-    if (i < 9) {
-      len += 2;
-    } else {
-      len -= 2;
-    }
+
+  const maxLineNum = isSymmetric ? 9 : 17;
+  for (let i = 0; i < maxLineNum; i++) {
+    const strLine = getLine(len);
+    len = i < 8 ? len + 2 : len - 2;
     str += strLine + "\n";
   }
   str = str.slice(0, -1);
-
   return str;
 }
 
